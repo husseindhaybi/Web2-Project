fba4d10259b8f4c7bd8c1cf01c1c2948dbd19de7
hussein dhaybi <you@example.com>
2025-11-19 20:04:52 +0200

    Create NabBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./components/About";
+import Cart from "./components/Cart";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Menu from "./components/Menu";
+import NavBar from "./components/Navbar";
+import OrderTracking from "./components/OrderTracking";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Router>
+      <div className="app-container">
+        <NavBar cartCount={cartItems.length} />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/menu" element={<Menu addToCart={addToCart} />} />
+            <Route
+              path="/cart"
+              element={
+                <Cart
+                  cartItems={cartItems}
+                  updateQuantity={updateQuantity}
+                  removeFromCart={removeFromCart}
+                  clearCart={clearCart}
+                />
+              }
+            />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/order-tracking" element={<OrderTracking />} />
+          </Routes>
+        </main>
+        
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,50 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Badge, Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../styles/navBar.css";
+
+function NavBar({ cartCount }) {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+      <Container>
+        <Navbar.Brand as={Link} to="/home">
+          <i className="bi bi-shop text-warning me-2" style={{ fontSize: '32px' }}></i>
+          Delicious Bites
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/home">
+              <i className="bi bi-house-fill me-1"></i>
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/menu">
+              <i className="bi bi-book-fill me-1"></i>
+              Menu
+            </Nav.Link>
+            
+            <Nav.Link as={Link} to="/about">
+              <i className="bi bi-info-circle-fill me-1"></i>
+              About
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact">
+              <i className="bi bi-telephone-fill me-1"></i>
+              Contact
+            </Nav.Link>
+            <Nav.Link as={Link} to="/cart" className="cart-link">
+              <i className="bi bi-cart-fill me-1"></i>
+              Cart
+              {cartCount > 0 && (
+                <Badge bg="danger" className="ms-2">
+                  {cartCount}
+                </Badge>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
