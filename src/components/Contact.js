780d7a015ac15101cf2c180d86cefec6fb0f303a
hussein dhaybi <you@example.com>
2025-11-19 20:57:47 +0200

    Preparing the Contact Page

---
+++ src/components/Contact.js
@@ -0,0 +1,39 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { useState } from "react";
+
+function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted:", formData);
+    setSubmitted(true);
+    
+    setTimeout(() => {
+      setSubmitted(false);
+      setFormData({ name: "", email: "", phone: "", message: "" });
+    }, 3000);
+  };
+
+  return (
+    <div>
+        
+    </div>
+  );
+}
+
+export default Contact;
